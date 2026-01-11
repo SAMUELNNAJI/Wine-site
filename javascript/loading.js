09b551ad94017120595ac9b750cebeab7700ad1a
SAMUELNNAJI <9jaflavzmedia@gmail.com>
2026-01-11 02:11:29 -0800

    did a loader

---
+++ javascript/loading.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        const loadingWrapper = document.querySelector('.loading-wrapper');
+        loadingWrapper.style.opacity = '0';
+        loadingWrapper.style.transition = 'opacity 0.6s ease';
+        setTimeout(() => {
+            loadingWrapper.style.display = 'none';
+        }, 800);
+    }, 1500);
+});
